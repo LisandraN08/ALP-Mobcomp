da67d2cf666b922463028416f12248c831b697c2
LisandraN08 <lnicoline@student.ciputra.ac.id>
2023-12-16 14:55:15 +0700

    Initial Commit

---
+++ ALP Mobcomp/ALP_MobcompApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MobcompApp.swift
+//  ALP Mobcomp
+//
+//  Created by MacBook Pro on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MobcompApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
